f9e635a55817eea8da63d5f7f01cc50acb488914
tangert <tyler@angert.com>
2018-04-04 17:38:55 -0400

    initial commit

---
+++ Pods/Target Support Files/Pods-reconote/Pods-reconote-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_reconote : NSObject
+@end
+@implementation PodsDummy_Pods_reconote
+@end
---
+++ Pods/Target Support Files/Pods-reconoteTests/Pods-reconoteTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_reconoteTests : NSObject
+@end
+@implementation PodsDummy_Pods_reconoteTests
+@end
